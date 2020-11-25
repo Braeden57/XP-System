601e886c14299a9d0034166b98d0ff2f0aa908a2
Braeden57 <sowinskibraeden@gmail.com>
2020-11-25 09:10:06 -0800

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+let dbPassword = 'mongodb+srv://client:backyardpassclient@backyard-pass-data.nbikf.mongodb.net/XPmockDB?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+var UserModel = require('../models/User.js');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, function(req, res) {
+    UserModel.find({}, function(err, data) {
+        res.render('dashboard', {
+            user : req.user,
+            members: data
+        });
+    });
+});
+
+// Member Profile
+router.get('/profile', ensureAuthenticated, function(req, res) {
+  UserModel.find({}, function(err, data) {
+    res.render('profile', {
+      memberProfile: req.eachMember
+    });
+  });
+});
+
+module.exports = router;
