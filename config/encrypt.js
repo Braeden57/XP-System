6dad94019faf84c226fae3bb76bccfc8973d377a
Braeden57 <sowinskibraeden@gmail.com>
2021-04-04 22:19:42 -0700

    Cleared default password

--- config/encrypt.js
+++ config/encrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 // Set `password` as the password you will use
 // Then Run this script and copy the output as it is your encrypted password
-let password = "APPLEZ57";
+let password = "";
 
 // Encrypts password and returns encrypted password
 bcrypt.genSalt(10, (err, salt) => {
