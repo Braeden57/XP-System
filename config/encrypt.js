f06c6f8757286480ac3adbdd4ec75b7b4d98a69b
Braeden57 <sowinskibraeden@gmail.com>
2021-04-04 22:30:04 -0700

    Updated Instructions

--- config/encrypt.js
+++ config/encrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 // Set `password` as the password you will use
 // Then Run this script and copy the output as it is your encrypted password
-// To run; open your CMD or Terminal in this directory, and use command 'node encrypt.js' 
+// To run; open your CMD or Terminal in this directory, and use command 'node encrypt.js'
 
 let password = "";
 
